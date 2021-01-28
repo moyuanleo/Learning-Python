d8ccd681cf8565dfd7199b5ca7c58ec5cdfeedeb
moyuanleo <1357474476@qq.com>
2021-01-28 17:32:36 +0800

    .\binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,26 @@
+'''
+Author: your name
+Date: 2021-01-26 21:13:42
+LastEditTime: 2021-01-26 21:20:36
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \Python\binary_search.py
+'''
+def binary_search(list,item):
+    low=0
+    high=len(list)-1
+
+    while low<=high:
+        mid=int ((low+high)/2)
+        guess=list[mid]
+        if guess ==item:
+            return mid
+        if guess>item:
+            high=mid-1
+        else:
+            low=mid+1
+    return None
+
+my_list =[1,3,5,7,9]
+print (binary_search(my_list,3))
+print (binary_search(my_list,-1)) 
