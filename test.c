25cde9ff9be3ff20b551dff9e663d6f260ed15ac
moyuanleo <1357474476@qq.com>
2021-01-29 15:17:42 +0800

    test.c

---
+++ test.c
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-29 15:15:09
+ * @LastEditTime: 2021-01-29 15:15:56
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Learning Python\test.c
+ */
+#include<stdio.h>
+int main(){
+    printf("test\n");
+    return 0;
+}
