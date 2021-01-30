ef6b00e253b41546af142e0bafd0aa6ab03ccd28
moyuanleo <1357474476@qq.com>
2021-01-30 21:37:14 +0800

    .\sanliebiao.py

---
+++ sanliebiao.py
@@ -0,0 +1,14 @@
+'''
+Author: your name
+Date: 2021-01-30 21:30:09
+LastEditTime: 2021-01-30 21:35:13
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \Learning Python\sanliebiao.py
+'''
+book = dict()
+book["apple"]=0.67
+book["milk"]=1.49
+book["avocado"]=1.49
+print (book= {'avocado':1.49, 'apple':0.67, 'milk':1.49})
+print(book["avocado"])
