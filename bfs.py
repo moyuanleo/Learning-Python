9522a0aaeae5f8c5e27ef64602b9f8e26b3ccd9a
moyuanleo <1357474476@qq.com>
2021-01-31 22:47:22 +0800

    .\bfs.py

--- bfs.py
+++ bfs.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2021-01-31 21:55:59
-LastEditTime: 2021-01-31 22:10:14
+LastEditTime: 2021-01-31 22:45:52
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: \Learning Python\bfs.py
@@ -14,7 +14,7 @@ graph["claire"] = ["thom","jonny"]
 graph["anuj"] = []
 graph["peggy"] = []
 graph["thom"] = []
-graph["jonny"] = []
+graph["jonny"] = []# ‘thom’、‘jonny’，‘peggy’没有邻居节点
 
 from collections import deque
 search_queue = deque()
@@ -22,18 +22,19 @@ search_queue += graph["you"]
 
 def search(name):
     search_queue = deque()
-    search_queue += graph[name]
-    searched = []
-    while search_queue:
-        person = search_queue.popleft()
+    search_queue += graph[name]#graph["you"]是一个数组，其中包含你的所有邻居
+    searched = []# 用于标记已经检查过的人
+    while search_queue:# 只要队列不为空
+        person = search_queue.popleft() # 从队列中弹出最左边的一个元素
         if person not in searched:
             if person_is_seller(person):
                 print (person + " is a mango seller!")
                 return True
             else:
-                search_queue += graph[person]
+                search_queue += graph[person]# 将当前节点的邻居放入队列中待检查
                 searched.append(person)
     return False
-def person_is_seller(name):
-    return name[-1] =="m"
+def person_is_seller(name):#判断是不是销售商的函数
+    return name[-1] =="m"#如果姓名以m结尾，就是
+
 search ("you")
