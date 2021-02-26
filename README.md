0c99d86a0492a1a73ca3113720d0cdc0ba38b9c5
leo <1357474476@qq.com>
2021-02-26 19:55:36 +0800

    .\README.md

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-02-22 17:00:24
+ * @LastEditTime: 2021-02-26 19:55:01
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \Learning Python\README.md
+-->
 # Learning-Python
 这是一个备份仓库
 用于存放学习Python所用的代码
+from Jan2020 to today
--- sanliebiao.py
+++ sanliebiao.py
@@ -1,14 +1,17 @@
 '''
 Author: your name
 Date: 2021-01-30 21:30:09
-LastEditTime: 2021-01-30 21:35:13
+LastEditTime: 2021-01-30 21:38:24
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: \Learning Python\sanliebiao.py
+'''
 '''
 book = dict()
 book["apple"]=0.67
 book["milk"]=1.49
 book["avocado"]=1.49
 print (book= {'avocado':1.49, 'apple':0.67, 'milk':1.49})
-print(book["avocado"])
+print(book["avocado"])'''
+book = {"apple": 0.67, "milk": 1.49, "avocado": 1.49}
+print(book)
