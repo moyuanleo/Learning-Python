a1394c283b28e4342388796eb135522ff90f8920
leo <1357474476@qq.com>
2021-03-02 12:58:52 +0800

    .\2_turtle.py

--- 1_turtle.py
+++ 1_turtle.py
@@ -1,3 +1,12 @@
+'''
+Author: moyuanleo
+Date: 2021-03-02 12:30:47
+LastEditTime: 2021-03-02 12:58:19
+LastEditors: Please set LastEditors
+Description: In User Settings Edit 第一次Python课的第一种画法：相对角度
+FilePath: \Learning Python\1_turtle.py
+'''
+
 import turtle as t
 t.fd(100)
 t.right(120)
---
+++ 2_turtle.py
@@ -0,0 +1,26 @@
+'''
+Author: moyuanleo
+Date: 2021-03-02 12:49:32
+LastEditTime: 2021-03-02 12:58:27
+LastEditors: Please set LastEditors
+Description: In User Settings Edit 第一次Python课的第二种画法
+FilePath: \Learning Python\2_turtle.py
+'''
+import turtle as t
+t.fd(100)
+t.seth(240)
+t.fd(100)
+t.seth(120)
+t.fd(100)
+t.seth(270)
+t.up()
+t.fd(150)
+t.down()
+t.fd(100)
+t.seth(360)
+t.fd(100)
+t.seth(90)
+t.fd(100)
+t.seth(180)
+t.fd(100)
+t.done()
