4db3c7ee2ff3ea40a3f19ec0b40f690beb4eb13b
leo <1357474476@qq.com>
2021-03-04 21:23:50 +0800

    .\sum&avg.py

---
+++ sum&avg.py
@@ -0,0 +1,17 @@
+'''
+Author: your name
+Date: 2021-03-04 21:18:32
+LastEditTime: 2021-03-04 21:23:33
+LastEditors: Please set LastEditors
+Description: In User Settings Edit练习2-1
+FilePath: \Learning Python\sum&ave.py
+'''
+m=eval(input("输入第一个数："))
+n=eval(input("输入第二个数："))
+
+s=m+n
+avg=(m+n)/2
+
+print("和为:",s)
+print("平均值为：",avg)
+#驼峰命名法
