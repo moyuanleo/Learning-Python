63ee2ed5050d10225595539872a407ce8a54343f
leo <1357474476@qq.com>
2021-03-04 21:17:38 +0800

    .\lianxi.py

---
+++ lianxi.py
@@ -0,0 +1,17 @@
+'''
+Author: moyuanleo
+Date: 2021-03-04 21:09:37
+LastEditTime: 2021-03-04 21:17:24
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \Learning Python\lianxi.py
+'''
+import turtle as t
+t.color("#00ffff")
+t.speed("fast")
+t.pensize(6)
+for x in range(20):
+    t.shape("turtle")
+    t.circle(100,360)
+    t.left(18)
+t.done()
