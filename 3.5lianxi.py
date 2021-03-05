400bdbdc76973f7c845bf0da7057b6b5c62a1811
leo <1357474476@qq.com>
2021-03-05 11:56:17 +0800

    .\3.5lianxi.py

---
+++ 3.5lianxi.py
@@ -0,0 +1,16 @@
+'''
+Author: xuqiming
+Date: 2021-03-05 11:36:47
+LastEditTime: 2021-03-05 11:48:24
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: \Learning Python\3.5lianxi.py
+'''
+class_name=input("请输入你的班级：")
+name=input("请输入你的姓名：")
+student_number=eval(input("请输入你的学号："))
+number_1,number_2=eval(input("请输入两个整数："))
+print("我的班级是：",class_name)
+print("我的姓名是：",name)
+print("我的学号是：",student_number)
+print(number_1,"+",number_2,"=",number_1+number_2)
