b79d5da64b02575efb00f2ab1a752d5e2eff3c2c
leo <1357474476@qq.com>
2021-04-18 10:33:16 +0800

    .

--- Assignments/shiyan6/2020331225sy6-8.py
+++ Assignments/shiyan6/2020331225sy6-8.py
@@ -1,5 +1,11 @@
 import random
 ls=['A','B','C','D','E']
-ls=random.shuffle(ls)
-if (ls[1]=='D')+(ls[2]=='B')==1 and (ls[1]=='C')+(ls[3]=='E')==1 and (ls[0]=='E')+(ls[4]=='A')==1 and (ls[2]=='C')+(ls[3]=='A')==1 and (ls[1]=='B')+(ls[4]=='D')==1:
-    print(ls)
+while True:
+    random.shuffle(ls)
+    if (ls[1]=='D')+(ls[2]=='B')==1\
+        and (ls[1]=='C')+(ls[3]=='E')==1\
+            and (ls[0]=='E')+(ls[4]=='A')==1\
+                and (ls[2]=='C')+(ls[3]=='A')==1\
+                    and (ls[1]=='B')+(ls[4]=='D')==1:
+        print(ls)
+        break
