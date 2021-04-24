05ea41e3195ee3d45edbf5cb0b827e99aff46fdc
leo <1357474476@qq.com>
2021-04-24 21:06:47 +0800

    .\rotate.py

---
+++ rotate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, nums, k):
+        # if len(nums)<2:
+        #     return
+        # nums.reverse()
+        # k=k%len(nums)
+        # while k>0:
+        #     temp=nums.pop(0)
+        #     nums.append(temp)
+        #     k-=1
+        # nums.reverse()
+        # return 
+
